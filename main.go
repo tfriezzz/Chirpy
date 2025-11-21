f98088ed3051165a44bff993bd8b708a388e1444
tfry <roswall.tim@gmail.com>
2025-11-21 17:27:15 +0900

    add /api/

--- main.go
+++ main.go
@@ -19,9 +19,9 @@ func main() {
 	}
 	var apiCfg apiConfig
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
-	mux.HandleFunc("GET /healthz", handlerReadiness)
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
-	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.Handle("GET /api/healthz", http.StripPrefix("/api/", http.HandlerFunc(handlerReadiness)))
+	mux.Handle("GET /api/metrics", http.StripPrefix("/api/", http.HandlerFunc(apiCfg.handlerMetrics)))
+	mux.Handle("POST /api/reset", http.StripPrefix("/api/", http.HandlerFunc(apiCfg.handlerReset)))
 
 	fmt.Printf("server listening on port %s\n", port)
 	if err := srv.ListenAndServe(); err != nil {
@@ -30,47 +30,26 @@ func main() {
 }
 
 func handlerReadiness(w http.ResponseWriter, r *http.Request) {
-	// fmt.Print("hi from handleReadiness")
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(200)
-	// j, _ := json.Marshal("OK")
 	w.Write([]byte("OK"))
 }
 
 func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
-	// fmt.Printf("hi form numberOfRequests")
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	hits := cfg.fileserverHits.Load()
 	strHits := fmt.Sprintf("Hits: %v", hits)
-	// fmt.Printf("test %v\n", hits)
 	w.WriteHeader(200)
-	// j, _ := json.Marshal(strHits)
 	w.Write([]byte(strHits))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
-	// return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// 	cfg.fileserverHits.Store(0)
-	// 	// next.ServeHTTP(w, r)
-	// })
-
 	cfg.fileserverHits.Store(0)
 }
 
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
-	// fmt.Println("hi from middlewareMetricsInc")
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits.Add(1)
 		next.ServeHTTP(w, r)
 	})
 }
-
-// func middleWareCfgWrap(handler func(cfg *apiConfig, w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
-// 	newHandler := func(w http.ResponseWriter, r *http.Request) {
-// 		handler(cfg, w, r)
-// 	}
-// 	fmt.Printf("hi from middleWareCfgWrap")
-//
-// 	return newHandler
-// }
